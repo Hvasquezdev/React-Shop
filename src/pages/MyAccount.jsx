86ddb320cd2a575b42d294d4b81a88d08421b657
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-06 19:01:26 -0300

    :sparkles:(my account): add readonly view and edit button behavior

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -8,6 +8,11 @@ import FormField from "../components/molecules/FormField";
 import Button from "../components/atoms/Button";
 
 function MyAccount() {
+  const [name, setName] = React.useState("Hector Vasquez");
+  const [email, setEmail] = React.useState("hvasquezdev@gmail.com");
+  const [password, setPassword] = React.useState("123456789");
+  const [editing, setEditing] = React.useState(false);
+
   return (
     <div className="my-account">
       <FormContainer>
@@ -17,27 +22,51 @@ function MyAccount() {
           action="/"
           formFields={() => (
             <>
-              <FormField id="name" label="Name" inputPlaceholder="Hector" />
+              <FormField
+                id="name"
+                label="Name"
+                inputPlaceholder="Hector"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                readOnly={!editing}
+                plainText={!editing}
+              />
               <FormField
                 id="email"
                 label="Email"
                 inputType="email"
                 inputPlaceholder="email@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                readOnly={!editing}
+                plainText={!editing}
               />
               <FormField
                 id="password"
                 label="Password"
                 inputType="password"
                 inputPlaceholder="*********"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                readOnly={!editing}
+                plainText={!editing}
               />
             </>
           )}
-          formActions={() => (
-            <Button type="submit" color="primary">
-              Create
-            </Button>
-          )}
+          formActions={() =>
+            editing && (
+              <Button type="submit" color="primary">
+                Create
+              </Button>
+            )
+          }
         />
+
+        {!editing && (
+          <Button color="primary" outlined onClick={() => setEditing(true)}>
+            Edit
+          </Button>
+        )}
       </FormContainer>
     </div>
   );
