914222c10fdc9a7352c8e1de1c8755b09fb4b602
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-05 21:33:57 -0300

    :sparkles: create form-container

---
+++ src/components/atoms/FormContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./../../assets/styles/atoms/FormContainer.scss";
+
+function FormContainer({ children }) {
+  return <div className="form-container">{children}</div>;
+}
+
+export default FormContainer;
