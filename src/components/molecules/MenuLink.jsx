1882dc623d81142a869a2c880952f2a09efce289
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-08 13:26:43 -0300

    :lipstick:(menu link): add font-size property

--- src/components/molecules/MenuLink.jsx
+++ src/components/molecules/MenuLink.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./../../assets/styles/molecules/MenuLink.scss";
 
-function MenuLink({ children, to, textAlign = "end" }) {
+import { getStringFromObjectKeys } from "../../utils";
+
+function MenuLink({ children, to, textAlign = "end", fontSize = "md" }) {
+  const classNames = getStringFromObjectKeys({
+    "menu-link": true,
+    [`menu-link--text-${textAlign}`]: textAlign,
+    [`menu-link--text-${fontSize}`]: fontSize,
+  });
+
   return (
-    <li className={`menu-link menu-link--text-${textAlign}`}>
+    <li className={classNames}>
       <Link to={to}>{children}</Link>
     </li>
   );
