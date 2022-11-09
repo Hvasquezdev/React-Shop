1d29539c618962fa4e7d9774e747db1d375c7623
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-09 19:06:55 -0300

    :sparkles:(Product card): handle product data and show it

--- src/components/organisms/ProductCard.jsx
+++ src/components/organisms/ProductCard.jsx
@@ -3,18 +3,18 @@ import "./../../assets/styles/organisms/ProductCard.scss";
 
 import cartIcon from "./../../assets/icons/bt_add_to_cart.svg";
 
-function ProductCard() {
+function ProductCard({ product }) {
   return (
-    <div class="product-card">
+    <div className="product-card">
       <img
         className="product-card__image"
-        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        alt="Product"
+        src={product.images[0] || ""}
+        alt={product.title}
       />
-      <div class="product-info">
+      <div className="product-info">
         <div className="product-info__text">
-          <p className="product-price">$120,00</p>
-          <p className="product-name">Bike</p>
+          <p className="product-price">${product.price}</p>
+          <p className="product-name">{product.title}</p>
         </div>
         <figure className="product-info__media">
           <img src={cartIcon} alt="Cart" />
