82470c6a151691f0db53c9a1ec5b8687c30e05e3
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-09 19:07:36 -0300

    :sparkles:(home): show products list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import ApiProductsService from "../core/services/Products.service";
 import Header from "../components/Header";
-import DesktopMenu from "../components/organisms/DesktopMenu";
+import ProductList from "../components/organisms/ProductList";
+
+const productsService = new ApiProductsService();
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await productsService.getProducts();
+    setProducts(response);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
       <Header />
+      <ProductList products={products} />
     </>
   );
 }
