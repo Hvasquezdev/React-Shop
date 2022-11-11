93ecc987983577d0d38bb2fb717a234485ce948b
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-11 20:45:16 -0300

    :sparkles: add order-card component

---
+++ src/assets/styles/molecules/OrderCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./../../styles/molecules/OrderCard.scss";
+
+import closeIcon from "./../../icons/icon_close.png";
+import Button from "../../../components/atoms/Button";
+
+function OrderCard() {
+  return (
+    <div className="order-card">
+      <figure className="order-card__media">
+        <img
+          src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="bike"
+          width="70px"
+          height="70px"
+          loading="lazy"
+          className="order-card__image"
+        />
+      </figure>
+
+      <p className="order-card__name">Bike</p>
+      <p className="order-card__price">$30,00</p>
+
+      <Button isIconButton>
+        <img
+          src={closeIcon}
+          alt="close"
+          className="order-card__icon"
+        />
+      </Button>
+    </div>
+  );
+}
+
+export default OrderCard;
