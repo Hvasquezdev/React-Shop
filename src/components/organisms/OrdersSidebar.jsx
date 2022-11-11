dd1310e311e5435a9fa93025f04ed017c89a942b
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-11 20:46:10 -0300

    :sparkles: add order-sidebar component

---
+++ src/components/organisms/OrdersSidebar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./../../assets/styles/organisms/OrdersSidebar.scss";
+
+import arrowIcon from "./../../assets/icons/flechita.svg";
+import BaseCard from "../atoms/BaseCard";
+import OrderCard from "../../assets/styles/molecules/OrderCard";
+import Button from "../atoms/Button";
+
+function OrdersSidebar() {
+  return (
+    <aside className="order-sidebar">
+      <div className="order-sidebar__head">
+        <img src={arrowIcon} alt="arrow" className="head-arrow" />
+        <p className="head-title">My order</p>
+      </div>
+
+      <div className="order-sidebar__content">
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+
+        <BaseCard className="order-sidebar__details">
+          <p className="total-text">Total</p>
+          <p className="total-amount">$560.00</p>
+        </BaseCard>
+
+        <Button color="primary">Checkout</Button>
+      </div>
+    </aside>
+  );
+}
+
+export default OrdersSidebar;
