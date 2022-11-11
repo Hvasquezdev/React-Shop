a691182eb0b735f23f6f37d37ab2ab6f886a0dbd
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-11 18:22:59 -0300

    :sparkles:(home): update app context by adding cart products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import Header from "../components/Header";
 import ProductList from "../components/organisms/ProductList";
+import useAppContext from "../hooks/useAppContext";
 import useProducts from "../hooks/useProducts";
 
 function Home() {
   const { products } = useProducts();
+  const { addProductToCart } = useAppContext();
 
   return (
     <>
       <Header />
-      <ProductList products={products} />
+      <ProductList products={products} onCartClick={addProductToCart} />
     </>
   );
 }
