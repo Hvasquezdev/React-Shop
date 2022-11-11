577d8ae772236de8f8c9513459c0789180a72fa2
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-10 22:03:09 -0300

    :sparkles:(utils): add FormData generator

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,20 @@
 const getStringFromObjectKeys = (obj = {}) => {
-  const clonedObj = {...obj};
-  const filteredKeys = Object.keys(clonedObj).filter(
-    (key) => !!clonedObj[key]
-  );
+  const clonedObj = { ...obj };
+  const filteredKeys = Object.keys(clonedObj).filter((key) => !!clonedObj[key]);
   return filteredKeys.join(" ");
 };
 
-export { getStringFromObjectKeys };
+const getFormDataFromObject = (obj = {}) => {
+  const form = new FormData();
+
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      const element = obj[key];
+      form.append(key, element);
+    }
+  }
+
+  return form;
+};
+
+export { getStringFromObjectKeys, getFormDataFromObject };
