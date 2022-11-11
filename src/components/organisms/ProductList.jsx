5fede740f7e5e967b98e50f4278a90e50f937d2c
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-11 18:22:35 -0300

    :sparkles:(product list): handle onCartClick method

--- src/components/organisms/ProductList.jsx
+++ src/components/organisms/ProductList.jsx
@@ -3,9 +3,16 @@ import "./../../assets/styles/organisms/ProductList.scss";
 
 import ProductCard from "./ProductCard";
 
-function ProductList({ products }) {
+function ProductList({ products, onCartClick }) {
+  const handleCartClick = (product) =>
+    onCartClick ? onCartClick(product) : null;
+
   const listOfProducts = [...products].map((product) => (
-    <ProductCard product={product} key={product.id} />
+    <ProductCard
+      product={product}
+      key={product.id}
+      onCartClick={handleCartClick}
+    />
   ));
 
   return <div className="product-list">{listOfProducts}</div>;
