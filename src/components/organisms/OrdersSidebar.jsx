47d54938a2097ac3d5884d01f7cbba99e15b464b
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-11 21:36:47 -0300

    :sparkles:(orders sidebar): show dynamic product list and hande onRemove event

--- src/components/organisms/OrdersSidebar.jsx
+++ src/components/organisms/OrdersSidebar.jsx
@@ -5,8 +5,17 @@ import arrowIcon from "./../../assets/icons/flechita.svg";
 import BaseCard from "../atoms/BaseCard";
 import OrderCard from "../molecules/OrderCard";
 import Button from "../atoms/Button";
+import ApiProductsService from "../../core/services/Products.service";
 
-function OrdersSidebar() {
+const productsService = new ApiProductsService();
+
+function OrdersSidebar({ products, onRemove }) {
+  const totalAmount = productsService.getTotalAmount(products);
+
+  const handleRemove = (productId) => {
+    return onRemove ? onRemove(productId) : null;
+  };
+
   return (
     <aside className="order-sidebar">
       <div className="order-sidebar__head">
@@ -15,13 +24,17 @@ function OrdersSidebar() {
       </div>
 
       <div className="order-sidebar__content">
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {products.map((product) => (
+          <OrderCard
+            product={product}
+            key={product.id}
+            onRemove={handleRemove}
+          />
+        ))}
 
         <BaseCard className="order-sidebar__details">
           <p className="total-text">Total</p>
-          <p className="total-amount">$560.00</p>
+          <p className="total-amount">${totalAmount}</p>
         </BaseCard>
 
         <Button color="primary">Checkout</Button>
